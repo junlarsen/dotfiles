b9e742e1f3ad6698476d65d909efd9df450f2b00
Mats Larsen <mats@jun.codes>
2023-07-23 10:00:54 +0200

    feat: bump anki version

--- config/zsh/.zshrc
+++ config/zsh/.zshrc
@@ -107,5 +107,5 @@ export JAVA_HOME="/home/jun/.jdks/openjdk-20.0.1"
 
 # Dotty PATH exports. Always keep this at the bottom of the file
 export PATH="/home/jun/dotty/packages/jetbrains-toolbox/jetbrains-toolbox-1.27.3.14493:$PATH"
-export PATH="/home/jun/dotty/packages/anki/anki-2.1.62-linux-qt6:$PATH"
 export PATH="/home/jun/dotty/packages/gradle/gradle-8.3/bin:$PATH"
+export PATH="/home/jun/dotty/packages/anki/anki-2.1.65-linux-qt6:$PATH"
--- packages/anki/install.sh
+++ packages/anki/install.sh
@@ -2,13 +2,13 @@
 
 sudo apt-get install -y libxcb-cursor0
 
-curl -L "https://github.com/ankitects/anki/releases/download/2.1.62/anki-2.1.62-linux-qt6.tar.zst" --compressed -o anki.zst
+curl -L "https://github.com/ankitects/anki/releases/download/2.1.65/anki-2.1.65-linux-qt6.tar.zst" --compressed -o anki.zst
 unzstd anki.zst -o anki.tar
 tar -xvf anki.tar
 
-sudo sh "$(pwd)/anki-2.1.62-linux-qt6/install.sh"
+sudo sh "$(pwd)/anki-2.1.65-linux-qt6/install.sh"
 
-TARGET="export PATH=\"$(pwd)/anki-2.1.62-linux-qt6:\$PATH\""
+TARGET="export PATH=\"$(pwd)/anki-2.1.65-linux-qt6:\$PATH\""
 grep -qxF "$TARGET" "$DOTTY_ENV" || echo "$TARGET" >> "$DOTTY_ENV"
 
-ln -s "$(pwd)/anki-2.1.62-linux-qt6/anki.desktop" "$HOME/.local/share/applications/anki.desktop"
+ln -s "$(pwd)/anki-2.1.65-linux-qt6/anki.desktop" "$HOME/.local/share/applications/anki.desktop"

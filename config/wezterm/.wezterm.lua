6801f20a79c2313c7894f384dc937c64bda56402
Mats Larsen <mats@jun.codes>
2024-05-20 19:25:46 +0200

    feat: add keybinds for window manipulation

--- config/wezterm/.wezterm.lua
+++ config/wezterm/.wezterm.lua
@@ -5,14 +5,32 @@ config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.keys = {
   {
     key = 'LeftArrow',
-    mods = 'CTRL|SHIFT',
+    mods = 'CTRL',
     action = wezterm.action.ActivateTabRelative(-1),
   },
   {
     key = 'RightArrow',
-    mods = 'CTRL|SHIFT',
+    mods = 'CTRL',
     action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    key = 'm',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitVertical {domain = "CurrentPaneDomain"},
+  },
+  {
+    key = 'n',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitHorizontal {domain = "CurrentPaneDomain"},
   }
 }
+
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'CTRL',
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
 
 return config

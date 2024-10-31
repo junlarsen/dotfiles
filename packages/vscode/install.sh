e4db4cca0fd2b47637d5147f0dcea8cb62d3e79f
Mats Larsen <mats@jun.codes>
2024-10-31 17:40:32 +0900

    fix: grab latest vscode available

--- packages/vscode/install.sh
+++ packages/vscode/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-curl -L --compressed "https://az764295.vo.msecnd.net/stable/704ed70d4fd1c6bd6342c436f1ede30d1cff4710/code_1.77.3-1681292746_amd64.deb" -o vscode.deb
+curl -L --compressed "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -o vscode.deb
 
 sudo dpkg -i vscode.deb

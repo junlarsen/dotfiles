93fc148215109c1e465cedcc31554bf8a46f0df5
Mats Jun Larsen <mats@jun.codes>
2025-01-27 21:44:02 +0900

    chore: add various utils

---
+++ packages/utils/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Installers for bat, fd, hyperfine, ripgrep, hexyl, difftastic
+
+cargo install --locked ripgrep
+cargo install --locked fd-find
+cargo install --locked bat
+cargo install --locked hyperfine
+cargo install --locked hexyl
+cargo install --locked difftastic

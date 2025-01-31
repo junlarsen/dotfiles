cb91bc9855d3ad009835333271671db000bd792d
Mats Jun Larsen <mats@jun.codes>
2025-02-01 02:49:51 +0900

    feat: add helix lsp installation for typst and cxx

--- bundles/helix.zsh
+++ bundles/helix.zsh
@@ -20,12 +20,25 @@ install_biome_lsp() {
 }
 
 install_terraform_lsp() {
+  dotfiles_log "Installing Terraform Language Server"
   curl -L --compressed https://releases.hashicorp.com/terraform-ls/0.36.4/terraform-ls_0.36.4_linux_amd64.zip -O "$TARGET/terraform-lsp/terraform-lsp.zip"
   unzip "$TARGET/terraform-lsp/terraform-lsp.zip" -d "$TARGET/terraform-lsp"
   cp "$TARGET/terraform-lsp/terraform-ls" ~/bin
+}
+
+install_cxx_lsp() {
+  dotfiles_log "Installing Clangd"
+  sudo apt install clangd-18
+}
+
+install_typst_lsp() {
+  dotfiles_log "Installing Tinymist (typst) LSP"
+  cargo install --git https://github.com/Myriad-Dreamin/tinymist --locked tinymist
 }
 
 install_helix
 install_terraform_lsp
 install_biome_lsp
 install_typescript_lsp
+install_cxx_lsp
+install_typst_lsp

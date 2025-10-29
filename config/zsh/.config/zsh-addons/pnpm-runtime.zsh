17923464b60d49c2d1bc3124c0719f917bdd874b
Mats Jun Larsen <mats@jun.codes>
2025-10-29 14:45:13 +0100

    fix: use rewritten pnpm path

--- config/zsh/.config/zsh-addons/pnpm-runtime.zsh
+++ config/zsh/.config/zsh-addons/pnpm-runtime.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-export PNPM_HOME="/home/jun/.local/share/pnpm"
+export PNPM_HOME="/home/mats/.local/share/pnpm"
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;

c5a4a0673de5703f2ab77eea698dca3ee75a8c00
Mats Jun Larsen <mats@jun.codes>
2025-12-30 13:12:41 +0100

    feat: add fluent queries

---
+++ config/helix/.config/helix/runtime/queries/fluent/highlights.scm
@@ -0,0 +1,15 @@
+; Comments
+(block_comment) @comment
+(comment) @comment
+(resource_comment) @comment
+
+; Messages and terms
+(message) @function
+(term) @constant
+
+; Identifiers
+(identifier) @variable
+(term_identifier) @constant
+
+; Patterns (string content)
+(pattern) @string

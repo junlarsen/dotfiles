00e1ba3dbcac3f68068da78b4c5aede3ee32ef70
Mats Jun Larsen <mats@jun.codes>
2026-07-01 10:03:01 +0200

    node: nuke old nvm configs

--- config/zsh/.config/zsh-addons/completions.zsh
+++ config/zsh/.config/zsh-addons/completions.zsh
@@ -6,7 +6,6 @@ autoload -U +X bashcompinit && bashcompinit
 command -v zoxide >/dev/null 2>&1 && eval "$(zoxide init zsh)"
 command -v starship >/dev/null 2>&1 && eval "$(starship init zsh)"
 command -v just >/dev/null 2>&1 && eval "$(just --completions zsh)"
-command -v pnpm >/dev/null 2>&1 && eval "$(pnpm completion zsh)"
 command -v wezterm >/dev/null 2>&1 && eval "$(wezterm shell-completion --shell zsh)"
 command -v docker >/dev/null 2>&1 && eval "$(docker completion zsh)"
 command -v packer >/dev/null 2>&1 && complete -o nospace -C /usr/bin/packer packer
--- config/zsh/.config/zsh-addons/nvm-runtime.zsh
+++ config/zsh/.config/zsh-addons/nvm-runtime.zsh
@@ -1,4 +1,3 @@
 #!/usr/bin/env zsh
-
 export NVM_DIR="/usr/share/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
